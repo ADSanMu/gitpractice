a02d93ac9a5acb32af7ef915f52e51aabbad3c47
Sanmu <1215148017@qq.com>
2017-11-11 09:56:17 +0800

    add HelloWorld class in machine no.2

---
+++ src/main/java/hello/HelloWorld.java
@@ -0,0 +1,12 @@
+package hello;
+
+/**
+ * @author Sanmu.
+ * @date 11/11/2017
+ * @email 1215148017@qq.com
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
